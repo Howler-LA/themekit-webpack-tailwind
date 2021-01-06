c60c5e510a04f90d4d343eae29a9c72e479cde0e
Craig Fowler <craig@howler.la>
2021-01-05 16:14:03 -0800

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   future: {
     removeDeprecatedGapUtilities: true,
@@ -6,7 +8,7 @@ module.exports = {
     standardFontWeights: true,
   },
   purge: {
-    enabled: true,
+    enabled: false,
     layers: ["components", "utilities"],
     content: [
       "./snippets/*.liquid",
@@ -16,8 +18,34 @@ module.exports = {
     ],
   },
   theme: {
-    extend: {},
+    aspectRatio: {
+      'none': 0,
+      'square': [1, 1],
+      '16/9':   [16, 9],
+      '4/3':    [4, 3],
+      '3/4':    [3, 4],
+      '21/9':   [21, 9],
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '2rem',
+        xl: '2rem',
+        '2xl': '6rem',
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   variants: {},
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/ui'),
+    require('tailwindcss-aspect-ratio'),
+    require('tailwindcss-blend-mode')()
+  ],
 };
